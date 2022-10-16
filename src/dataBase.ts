6685e89d8d10e4a1d00592c38f9dcec68abfb241
Antonio <antonio.garciamn@gmail.com>
2022-10-16 18:34:58 -0300

    feat: mutation installStation

--- src/dataBase.ts
+++ src/dataBase.ts
@@ -9,7 +9,6 @@ async function getPlanets() {
 		const { data: planets } = await axios.get(
 			"https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_masse,pl_bmassj+from+ps+where+pl_bmassj > 10&format=json"
 		);
-		console.log(planets);
 		const planetsSetPros = await planets.map((p) => {
 			return { name: p.pl_name, mass: p.pl_masse, hasStation: false };
 		});
--- src/modules/resolvers.ts
+++ src/modules/resolvers.ts
@@ -1,4 +1,11 @@
 import getPlanets from "../dataBase.js";
+import { prisma } from "../dataBase.js";
+
+interface planet {
+	name: string;
+	mass: number;
+	hasStation: boolean;
+}
 
 const resolvers = {
 	Query: {
@@ -9,8 +16,12 @@ const resolvers = {
 	},
 
 	Mutation: {
-		installStation(_: any, args) {
-			console.log(args);
+		async installStation(_: any, args: planet) {
+			const response = await prisma.stations.create({
+				data: { planet: args.name },
+			});
+			console.log(response);
+			return args;
 		},
 	},
 };
